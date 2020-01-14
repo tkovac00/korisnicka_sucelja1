e0c17299d4a8f0793f0ea66b1f408d34726fda85
tkovac00 <tkovac00@fesb.hr>
2020-01-14 11:21:56 +0100

    commit

---
+++ src/pages/Products/Accessories.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from '../../components/Layout/layout'
+import Head from '../../components/head'
+
+const AccessoriesPage = () => (
+ <Layout>
+   <Head title="Clothes" />
+   <div>
+      <h1 >Clothes</h1>
+   </div>
+    </Layout> 
+  )
+  
+  export default AccessoriesPage
+ 
