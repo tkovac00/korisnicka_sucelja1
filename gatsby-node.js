2381a638dd369070ba46d051e3e1c642bbeba18a
tkovac00 <tkovac00@fesb.hr>
2020-01-14 11:01:46 +0100

    second commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const path = require('path')
 
  module.exports.createPages = async({ graphql, actions }) => {
     const { createPage } = actions
-    const blogTemplate = path.resolve('./src/templates/blog.js')
+    const blogTemplate = path.resolve('./src/templates/Blog.js')
     const res = await graphql(`
         query {
           allContentfulBlogPost {
