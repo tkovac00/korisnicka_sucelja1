a7444ee96eef2749fe93327617c25dffd8a1d59a
tkovac00 <tkovac00@fesb.hr>
2020-02-18 23:00:17 +0100

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const HomePage = () => {
       <Head title="Home"/>
       <Showcase />
     
-    <div style={{width: '750px', marginLeft: 'auto', marginRight:'auto',display: 'flex',flexDirection: 'column',
+    <div style={{width:'100%', marginLeft: 'auto', marginRight:'auto',display: 'flex',flexDirection: 'column',
     paddingTop: '20px'}}><p>Lorem Ipsum is simply dummy text of
         the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when 
         an unknown printer took a galley of type and scrambled it to make a type specimen book. ItLorem Ipsum is simply dummy text
